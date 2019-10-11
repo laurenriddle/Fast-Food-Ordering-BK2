cf1bd497808e193a048d42024b258f5d2be7a47a
Lauren Riddle <lari7132@gmail.com>
2019-10-11 15:55:28 -0500

    added js functions to place and get orders - version 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
-console.log("Test")
+const restaurant = {
+    name: "Bob's Burgers",
+    placeOrder: function (meal) {
+       this.orders.push(meal)
+    },
+    orders: [],
+    getOrders: function () {
+        return this.orders
+    }
+
+}
+
+
+const chickenComboMeal = {
+    sandwichType: "Chicken Sandwitch",
+    fries: true,
+    drinkSize: "Medium"
+}
+
+// Place an order
+restaurant.placeOrder(chickenComboMeal)
+
+// Invoke the function to return the list of all orders
+console.log(restaurant.getOrders())
+// Output all orders to the console using console.table()
